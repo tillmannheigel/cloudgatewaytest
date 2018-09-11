a10c66dd5a310461b40c0ed21cddd74d8b2422bf
Tillmann Heigel <tillmannheigel@gmail.com>
2018-09-11 21:29:53 +0200

    Example with spring-cloud-starter-netflix-ribbon and RibbonClients

--- gateway/src/main/java/com/garethwestern/playground/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/garethwestern/playground/gateway/GatewayApplication.java
@@ -1,13 +1,28 @@
 package com.garethwestern.playground.gateway;
 
+import com.netflix.loadbalancer.Server;
+import com.netflix.loadbalancer.ServerList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
+import org.springframework.cloud.netflix.ribbon.StaticServerList;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@RibbonClients({
+        @RibbonClient(name = "service", configuration = GatewayApplication.TestRibbonConfig.class),
+})
 public class GatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+    protected static class TestRibbonConfig {
 
+        @Bean
+        public ServerList<Server> ribbonServerList() {
+            return new StaticServerList<>(new Server("localhost", 8081));
+        }
+    }
 }
